0aecfdf6fcfe1ffb468f4b31ddb404aa7f6a0e0e
S Sharanya Bharghavi <ssharanyab@gmail.com>
2022-04-07 22:16:54 +0530

    minor change

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -10,115 +10,115 @@ class Dashboard extends StatelessWidget {
     double width = MediaQuery.of(context).size.width;
     return SafeArea(
       child: Scaffold(
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              padding: const EdgeInsets.only(
-                left: 30,
-                bottom: 30,
-                right: 20,
-                top: 20,
-              ),
-              height: height * 0.3,
-              width: width,
-              decoration: BoxDecoration(
-                color: Colors.blue,
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(30),
-                  bottomRight: Radius.circular(30),
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                padding: const EdgeInsets.only(
+                  left: 30,
+                  bottom: 30,
+                  right: 20,
+                  top: 20,
+                ),
+                height: height * 0.3,
+                width: width,
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(30),
+                    bottomRight: Radius.circular(30),
+                  ),
+                ),
+                child: Stack(
+                  children: [
+                    Align(
+                      child: Text(
+                        'Welcome to Mumbai!',
+                        style: TextStyle(
+                          fontSize: 25,
+                        ),
+                      ),
+                      alignment: Alignment.bottomLeft,
+                    ),
+                    Align(
+                      child: Icon(Icons.settings),
+                      alignment: Alignment.topRight,
+                    ),
+                  ],
                 ),
               ),
-              child: Stack(
-                children: [
-                  Align(
-                    child: Text(
-                      'Welcome to Mumbai!',
-                      style: TextStyle(
-                        fontSize: 25,
+              Container(
+                padding: EdgeInsets.only(left: 20, top: 20),
+                child: Text(
+                  'Explore',
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(left: 20, top: 20, right: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      height: 75,
+                      width: 75,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.red,
                       ),
+                      child: Icon(Icons.location_on),
                     ),
-                    alignment: Alignment.bottomLeft,
-                  ),
-                  Align(
-                    child: Icon(Icons.settings),
-                    alignment: Alignment.topRight,
-                  ),
-                ],
+                    Container(
+                      height: 75,
+                      width: 75,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.red,
+                      ),
+                      child: Icon(Icons.location_on),
+                    ),
+                    Container(
+                      height: 75,
+                      width: 75,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.red,
+                      ),
+                      child: Icon(Icons.location_on),
+                    ),
+                    Container(
+                      height: 75,
+                      width: 75,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.red,
+                      ),
+                      child: Icon(Icons.location_on),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            Container(
-              padding: EdgeInsets.only(left: 20, top: 20),
-              child: Text(
-                'Explore',
-                style: TextStyle(fontSize: 20),
+              Padding(
+                padding: EdgeInsets.only(left: 20, top: 20, right: 20),
+                child: Container(
+                  height: 50,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(20),
+                      ),
+                      color: Colors.grey),
+                ),
               ),
-            ),
-            Container(
-              padding: EdgeInsets.only(left: 20, top: 20, right: 20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(
-                    height: 75,
-                    width: 75,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Colors.red,
-                    ),
-                    child: Icon(Icons.location_on),
-                  ),
-                  Container(
-                    height: 75,
-                    width: 75,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Colors.red,
-                    ),
-                    child: Icon(Icons.location_on),
-                  ),
-                  Container(
-                    height: 75,
-                    width: 75,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Colors.red,
-                    ),
-                    child: Icon(Icons.location_on),
-                  ),
-                  Container(
-                    height: 75,
-                    width: 75,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Colors.red,
-                    ),
-                    child: Icon(Icons.location_on),
-                  ),
-                ],
+              Container(
+                padding: EdgeInsets.only(left: 20, top: 20),
+                child: Text(
+                  'Popular',
+                  style: TextStyle(fontSize: 20),
+                ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(left: 20, top: 20, right: 20),
-              child: Container(
-                height: 50,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(20),
-                    ),
-                    color: Colors.grey),
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.only(left: 20, top: 20),
-              child: Text(
-                'Popular',
-                style: TextStyle(fontSize: 20),
-              ),
-            ),
-          ],
-        ),
-      ),
+            ],
+          ),
+
     );
   }
 }
