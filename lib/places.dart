657054822f65746e4298ffe5d62d489969d9672f
S Sharanya Bharghavi <ssharanyab@gmail.com>
2022-04-20 22:18:18 +0530

    commit

---
+++ lib/places.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class places extends StatefulWidget {
+  const places({Key? key}) : super(key: key);
+
+  @override
+  State<places> createState() => _placesState();
+}
+
+class _placesState extends State<places> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
